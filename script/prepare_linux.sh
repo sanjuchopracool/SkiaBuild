18c4f1bfd995bd01826959c8222f1808278e5660
sanjuchopracool <sanjuchopracool@gmail.com>
2024-05-30 19:28:07 +0530

    Update prepare_linux.sh

--- script/prepare_linux.sh
+++ script/prepare_linux.sh
@@ -8,7 +8,7 @@ apt-get update -y
 apt-get install build-essential software-properties-common -y
 apt-get update
 apt-get install gcc -y
-sudo apt-get install gcc-aarch64-linux-gnu -y
+sudo apt-get install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu -y
 
 apt-get install git python3 wget -y
 apt-get install ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip -y
