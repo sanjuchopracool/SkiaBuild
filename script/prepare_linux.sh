c9b9e389b8dbb11c9c8d6efe22f481248207597d
sanjuchopracool <sanjuchopracool@gmail.com>
2024-05-30 17:28:51 +0530

    Update prepare_linux.sh

--- script/prepare_linux.sh
+++ script/prepare_linux.sh
@@ -7,9 +7,7 @@ add-apt-repository ppa:ubuntu-toolchain-r/test -y
 apt-get update -y
 apt-get install build-essential software-properties-common -y
 apt-get update
-apt-get install gcc-9 g++-9 -y
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
-update-alternatives --config gcc 
+apt-get install gcc -y
 
 apt-get install git python3 wget -y
 apt-get install ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip -y
