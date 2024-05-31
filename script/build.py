5675cea8f1086d08cc099011198352b3ba871a4f
Sanjay <sanjuchopracool@gmail.com>
2024-05-31 11:08:00 +0530

    checking new config

--- script/build.py
+++ script/build.py
@@ -17,25 +17,25 @@ def main():
 
   args += [
     'target_cpu="' + machine + '"',
-    'skia_use_system_expat=false',
-    'skia_use_system_libjpeg_turbo=false',
-    'skia_use_system_libpng=false',
-    'skia_use_system_libwebp=false',
-    'skia_use_system_zlib=false',
-    'skia_use_sfntly=false',
-    'skia_use_freetype=true',
-    # 'skia_use_harfbuzz=true',
-    'skia_use_system_harfbuzz=false',
-    'skia_pdf_subset_harfbuzz=true',
-    # 'skia_use_icu=true',
-    'skia_use_system_icu=false',
-    # 'skia_enable_skshaper=true',
-    # 'skia_enable_svg=true',
-    'skia_enable_skottie=true'
   ]
 
   if 'macos' == system:
     args += [
+      'skia_use_system_expat=false',
+      'skia_use_system_libjpeg_turbo=false',
+      'skia_use_system_libpng=false',
+      'skia_use_system_libwebp=false',
+      'skia_use_system_zlib=false',
+      'skia_use_sfntly=false',
+      'skia_use_freetype=true',
+      # 'skia_use_harfbuzz=true',
+      'skia_use_system_harfbuzz=false',
+      'skia_pdf_subset_harfbuzz=true',
+      # 'skia_use_icu=true',
+      'skia_use_system_icu=false',
+      # 'skia_enable_skshaper=true',
+      # 'skia_enable_svg=true',
+      'skia_enable_skottie=true'
       'skia_use_system_freetype2=false',
       # 'skia_enable_gpu=true',
       'skia_use_metal=true',
@@ -47,6 +47,21 @@ def main():
       args += ['extra_cflags=["-stdlib=libc++", "-mmacosx-version-min=10.13"]']
   elif 'linux' == system:
     args += [
+      'skia_use_system_expat=false',
+      'skia_use_system_libjpeg_turbo=false',
+      'skia_use_system_libpng=false',
+      'skia_use_system_libwebp=false',
+      'skia_use_system_zlib=false',
+      'skia_use_sfntly=false',
+      'skia_use_freetype=true',
+      # 'skia_use_harfbuzz=true',
+      'skia_use_system_harfbuzz=false',
+      'skia_pdf_subset_harfbuzz=true',
+      # 'skia_use_icu=true',
+      'skia_use_system_icu=false',
+      # 'skia_enable_skshaper=true',
+      # 'skia_enable_svg=true',
+      'skia_enable_skottie=true'
       'skia_use_system_freetype2=true',
       # 'skia_enable_gpu=true',
       'extra_cflags_cc=["-frtti"]',
@@ -66,10 +81,15 @@ def main():
 
   elif 'windows' == system:
     args += [
-      'skia_use_system_freetype2=false',
-      # 'skia_use_angle=true',
-      # 'skia_use_direct3d=true',
-      'extra_cflags=["-DSK_FONT_HOST_USE_SYSTEM_SETTINGS"]',
+        'skia_use_system_libjpeg_turbo=false',
+        'skia_use_system_zlib=false',
+        'skia_use_system_harfbuzz=false',
+        'skia_use_system_libpng=false',
+        'skia_use_system_libwebp=false',
+        'skia_use_system_icu=false',
+        'skia_use_system_expat=false',
+    ]
+    args += [
     ]
   elif 'android' == system:
     args += [
